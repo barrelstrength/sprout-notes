b46570d4346a8e58e1bdb9bc439b5948750cb46a
Ben Parizek <ben@barrelstrengthdesign.com>
2015-12-01 02:02:44 -0800

    Updates plugin to support Craft 2.5
    - Adds icon
    - Adds icon mask
    - Adds Description
    - Adds Documentation URL
    - Adds Release Feed URL

--- SproutNotesPlugin.php
+++ SproutNotesPlugin.php
@@ -3,28 +3,43 @@ namespace Craft;
 
 class SproutNotesPlugin extends BasePlugin
 {
-    public function getName()
-    {
-        return Craft::t('Notes');
-    }
+	public function getName()
+	{
+	  return Craft::t('Notes');
+	}
 
-    public function getVersion()
-    {
-        return '0.6.0';
-    }
+	public function getDescription()
+	{
+	  return 'Add notes to your dashboard.';
+	}
 
-    public function getDeveloper()
-    {
-        return 'Barrel Strength Design';
-    }
+	public function getVersion()
+	{
+	  return '0.6.0';
+	}
 
-    public function getDeveloperUrl()
-    {
-        return 'http://barrelstrengthdesign.com';
-    }
+	public function getDeveloper()
+	{
+	  return 'Barrel Strength Design';
+	}
 
-    public function hasCpSection()
-    {
-        return false;
-    }
+	public function getDeveloperUrl()
+	{
+	  return 'http://barrelstrengthdesign.com';
+	}
+
+	public function getDocumentationUrl()
+	{
+	  return 'http://sprout.barrelstrengthdesign.com/craft-plugins/notes/docs';
+	}
+
+	public function getReleaseFeedUrl()
+	{
+	  return 'http://sprout.barrelstrengthdesign.com/craft-plugins/notes/releases.json';
+	}
+
+	public function hasCpSection()
+	{
+	  return false;
+	}
 }
