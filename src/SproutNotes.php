6f6e5f806a3275e39ebf903823bec8fe3e35c0e4
Ben Parizek <ben@barrelstrengthdesign.com>
2017-11-23 01:03:19 -0500

    Removes unused code

--- src/SproutNotes.php
+++ src/SproutNotes.php
@@ -11,23 +11,11 @@ use barrelstrength\sproutnotes\widgets\Notes as NotesWidget;
 class SproutNotes extends Plugin
 {
     /**
-     * Enable use of SproutNotes::$app-> in place of Craft::$app->
-     *
-     * Craft::$app->
-     * SproutNotes::$app-> Plugin Object
-     *
-     * @var $app SproutNotes
-     */
-    public static $app;
-
-    /**
      * @inheritdoc
      */
     public function init()
     {
         parent::init();
-
-        self::$app = $this;
 
         Event::on(Dashboard::class, Dashboard::EVENT_REGISTER_WIDGET_TYPES, function(RegisterComponentTypesEvent $event) {
             $event->types[] = NotesWidget::class;
